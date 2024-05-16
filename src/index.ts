c71a5797b59732c7d395d591ec79e2d662012518
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-05-15 20:06:41 -0700

    fixes

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,19 @@ import fs from 'fs';
 import path from 'path';
 import { MarkdownConverter } from './MarkdownConverter';
 
-(async () => {
-  const mdConverter = new MarkdownConverter();
+const DOCS_DIR = path.resolve(__dirname, '../docs');
+const PDFS_DIR = path.resolve(__dirname, '../pdfs');
+const OUTPUT_DIR = path.resolve(__dirname, './output');
 
-  // Get all top level directories
-  const directories = fs
-    .readdirSync(path.resolve(__dirname, '../docs'), { withFileTypes: true })
+const ensureDirectoryExists = (dir: string): void => {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+};
+
+const getDirectories = (baseDir: string): string[] => {
+  return fs
+    .readdirSync(baseDir, { withFileTypes: true })
     .filter(
       (dirent) =>
         dirent.isDirectory() &&
@@ -15,26 +22,40 @@ import { MarkdownConverter } from './MarkdownConverter';
         !dirent.name.startsWith('assets'),
     )
     .map((dirent) => dirent.name);
+};
+
+const cleanUpPdfs = (baseDir: string): void => {
+  const pdfs = fs
+    .readdirSync(baseDir, { withFileTypes: true })
+    .filter((dirent) => dirent.isFile() && dirent.name.endsWith('.pdf'))
+    .map((dirent) => dirent.name);
+
+  pdfs.forEach((pdf) => fs.unlinkSync(path.resolve(baseDir, pdf)));
+};
+
+const convertDirectory = async (
+  dir: string,
+  mdConverter: MarkdownConverter,
+): Promise<void> => {
+  const dirPath = path.resolve(DOCS_DIR, dir);
+  await mdConverter.convertAllMarkdownsInDirectory(dirPath);
+  ensureDirectoryExists(OUTPUT_DIR); // Ensure the output directory exists
+  await mdConverter.mergePdfsInDirectory(PDFS_DIR, `${OUTPUT_DIR}/${dir}.pdf`);
+  cleanUpPdfs(PDFS_DIR);
+};
+
+const main = async () => {
+  const mdConverter = new MarkdownConverter();
+  const directories = getDirectories(DOCS_DIR);
+  const regex = new RegExp(process.argv[2]);
 
   console.log('Scanning directories: ', directories);
 
-  // Loop through each directory and convert markdown files
   for (const dir of directories) {
-    await mdConverter.convertAllMarkdownsInDirectory(
-      path.resolve(__dirname, '../docs', dir), // Corrected path
-    );
-
-    await mdConverter.mergePdfsInDirectory('pdfs', `./output/${dir}.pdf`);
-
-    // cleanup all pdfs generated individually
-
-    const pdfs = fs
-      .readdirSync(path.resolve(__dirname, '../pdfs'), { withFileTypes: true })
-      .filter((dirent) => dirent.isFile() && dirent.name.endsWith('.pdf'))
-      .map((dirent) => dirent.name);
-
-    for (const pdf of pdfs) {
-      fs.unlinkSync(path.resolve(__dirname, '../pdfs', pdf));
+    if (regex.test(dir)) {
+      await convertDirectory(dir, mdConverter);
     }
   }
-})();
+};
+
+main();
